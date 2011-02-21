7a61eba325f9c0ab7bde7b6581ccdec1d272cb5f
Tom Cobb <Tom.Cobb@diamond.ac.uk>
2011-02-21 09:43:47 +0000

    agilent33220A: added simulation

--- etc/simulations/agilent33220A_sim.py
+++ etc/simulations/agilent33220A_sim.py
@@ -1,19 +1,48 @@
+#!/bin/env dls-python2.6
 from pkg_resources import require
-require("dls_serial_sim==1.15")
+require("dls_serial_sim")
 
-from dls_serial_sim import serial_device
+from dls_serial_sim import serial_device, CreateSimulation
 
 class agilent33220A_sim(serial_device):
 
-    Terminator = "\\n"
+    Terminator = "\n"    
 
     def __init__(self):
         # place your initialisation code here
         serial_device.__init__(self)
+        self.values = {
+        	"FUNC": "SIN",
+        	"FREQ": 1000.0,
+        	"VOLT": 10.0,
+        	"VOLT:OFFS": 5.0,
+        	"OUTP": 0,
+        	"FUNC:SQU:DCYC": 50.0,
+        	"FUNC:RAMP:SYMM": 50.0,
+        	"FUNC:PULS:WIDT": 0.000001,
+        	"FUNC:PULS:TRAN": 0.000000005,
+        }        	
 
     def reply(self,command):
         # reply to commands here
-        return command
+        if command.endswith("?"):
+        	key = command.rstrip("?")
+        	value = None
+        else:
+        	key, value = command.split(" ",1)
+        if key in self.values:
+        	typ = type(self.values[key])
+        else:
+        	return
+        if value is None:
+        	if typ == str:
+        		return self.values[key]
+        	elif typ == int:
+        		return "%d" % self.values[key]
+        	elif typ == float:
+        		return "%+.11E" % self.values[key]
+        else:
+        	self.values[key] = typ(value)
 
 if __name__=="__main__":
     # run our simulation on the command line. Run this file with -h for help
